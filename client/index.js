4a846c3aca9eef9e683ea284a3bab36bdb343da9
HypeDis <m.bae@live.com>
2019-08-13 23:57:05 -0400

    front end config

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const appDiv = document.getElementById('app');
+ReactDOM.render(<div>hello</div>, appDiv);
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const path = require('path');
 
 const redditRoutes = require('./apiRoutes/redditRoutes');
 const summaryRoutes = require('./apiRoutes/summaryRoutes');
@@ -12,8 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
+const publicPath = path.join(__dirname, '/public');
 app.use('/api/reddit', redditRoutes);
 app.use('/api/summary', summaryRoutes);
+app.use(express.static(publicPath));
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 db.sync()
   .then(() => {
